075c6ea925ac7c2b98ed76ba0bb6095445d164ba
Pedro Yves Fracari Ceripes <pedro.yves.fracari.ceripes@ipt.fraunhofer.de>
2022-11-23 14:12:57 +0100

    Add other AMM on the plot

--- pages/Stable curve.py
+++ pages/Stable curve.py
@@ -2,44 +2,8 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
-from gql import gql, Client
-from gql.transport.requests import RequestsHTTPTransport
-
-subgraph_url = "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer-v2"
-balancer_transport=RequestsHTTPTransport(
-    url=subgraph_url,
-    verify=True,
-    retries=3
-)
-client = Client(transport=balancer_transport)
-
-def get_D_binary_pool(x, y, amp):
-  a = 1/4
-  b = amp
-  c = -1 * ((amp * (x+y)) + (x*y))
-  delta = ((b**2) - (4*a*c))**(1/2) 
-  return (-b + delta)/(2*a)
-
-def get_y_binary_pool(x, amp, D):
-  first_part = amp*D + ((D**2)/4) - amp*x
-  second_part = (amp+x)**(-1)
-  return first_part * second_part
-
-def query_pool_data(pool_id):
-  query = '''
-  query {{
-      pools(where: {{ id: "{pool_id}" }}) {{
-          amp
-          name
-      }}
-      poolTokens( where: {{ poolId: "{pool_id}" }}) {{
-          id
-              name
-              balance
-      }}
-  }}
-  '''
-  return client.execute(gql(query.format(pool_id=pool_id)))
+from utils.queries import query_pool_data
+from utils.market_makers import LinearInvariant, Uniswap, StableSwap
 
 st.title('Stable curve simulation')
 pool_id = st.text_input('Pool id', value='0x2d011adf89f0576c9b722c28269fcb5d50c2d17900020000000000000000024d')
@@ -55,20 +19,30 @@ base_x = float(x_data["balance"])
 base_y = float(x_data["balance"])
 base_amp = float(x_data["balance"])
 
-amountX = st.slider(x_data["name"], min_value=base_x/100, max_value=base_x*2, value=base_x, step=base_x/100)
-amountY = st.slider(y_data["name"], min_value=base_y/100, max_value=base_y*2, value=base_y, step=base_y/100)
+# amountX = st.slider(x_data["name"], min_value=base_x/100, max_value=base_x*2, value=base_x, step=base_x/100)
+# amountY = st.slider(y_data["name"], min_value=base_y/100, max_value=base_y*2, value=base_y, step=base_y/100)
 amountAmp = st.slider('Amp factor', min_value=base_amp/100, max_value=base_amp*2, value=base_amp, step=base_amp/100)
 
-D = get_D_binary_pool(amountX, amountY, amountAmp)
+stable_swape = StableSwap(x=base_x, y=base_y, amp=base_amp)
+linear_invariant = LinearInvariant(x=base_x, y=base_y)
+uniswap = Uniswap(x=base_x, y=base_y)
 
-st.write('D is equal to ', D)
+st.write('Constant value of the pool is equal to ', stable_swape.constant)
 
 df = pd.DataFrame()
 
-df[x_data["name"]] = np.linspace(amountX/2, 2*amountX, num=1000)
-df[y_data["name"]] = get_y_binary_pool(df[x_data["name"]], amountAmp, D)
-df["Price"] = df[y_data["name"]] / df[x_data["name"]]
+df[x_data["name"]] = np.linspace(linear_invariant.constant/1000, linear_invariant.constant, num=1000)
+df["StableSwap"] = stable_swape.calculate_y(df[x_data["name"]], amountAmp)
+df["Linear Invariant"] = linear_invariant.calculate_y(df[x_data["name"]])
 
-fig = px.line(df, x=x_data["name"], y=y_data["name"], hover_data=["Price"], title='StableSwap plot')
+df["Uniswap"] = uniswap.calculate_y(df[x_data["name"]])
+df["Price StableSwap"] = df["StableSwap"] / df[x_data["name"]]
+df["Price Linear Invariant"] = df["Linear Invariant"] / df[x_data["name"]]
+df["Price Uniswap"] = df["Uniswap"] / df[x_data["name"]]
+
+fig = px.line(df, x=x_data["name"], y=["StableSwap", "Linear Invariant", "Uniswap"], 
+              hover_data=["Price StableSwap", "Price Linear Invariant", "Price Uniswap"], title='StableSwap plot')
+
+fig.update_layout(yaxis_title=y_data["name"])
 
 st.plotly_chart(fig, use_container_width=True)
