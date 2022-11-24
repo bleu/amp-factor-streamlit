2dbe84dffe30e625ecb4231c131b57c7ad55fc14
Pedro Yves Fracari Ceripes <pedro.yves.fracari.ceripes@ipt.fraunhofer.de>
2022-11-24 10:33:27 +0100

    Optimize Subgraph query

--- pages/3 Stable curve.py
+++ pages/3 Stable curve.py
@@ -2,25 +2,24 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import numpy as np
-from utils.queries import query_pool_data
+from utils.queries import Subgraph
 from utils.market_makers import LinearInvariant, Uniswap, StableSwap
 
 st.title('Stable curve simulation')
 pool_id = st.text_input('Pool id', value='0x2d011adf89f0576c9b722c28269fcb5d50c2d17900020000000000000000024d')
 
-response = query_pool_data(pool_id)
-pool_data = response["pools"][0]
-x_data = response["poolTokens"][0]
-y_data = response["poolTokens"][1]
+subgraph = Subgraph()
+response = subgraph.query_pool_by_id(pool_id)
+pool_data = response["pool"]
+x_data = pool_data["tokens"][0]
+y_data = pool_data["tokens"][1]
 
-st.write('Pool Name: ', pool_data["name"])
+st.subtitle(pool_data["name"])
 
 base_x = float(x_data["balance"])
 base_y = float(x_data["balance"])
 base_amp = float(x_data["balance"])
 
-# amountX = st.slider(x_data["name"], min_value=base_x/100, max_value=base_x*2, value=base_x, step=base_x/100)
-# amountY = st.slider(y_data["name"], min_value=base_y/100, max_value=base_y*2, value=base_y, step=base_y/100)
 amountAmp = st.slider('Amp factor', min_value=base_amp/100, max_value=base_amp*2, value=base_amp, step=base_amp/100)
 
 stable_swape = StableSwap(x=base_x, y=base_y, amp=base_amp)
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 from .market_makers import LinearInvariant, Uniswap, StableSwap
-from .queries import query_pool_data
+from .queries import Subgraph
