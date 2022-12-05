ee49634a6abc92ec04271c40cfa80f0aa4cb441b
luizakp <luiza.puccinelli06@gmail.com>
2022-12-05 09:11:34 +0100

    add update session state

--- utils/streamlit.py
+++ utils/streamlit.py
@@ -3,4 +3,6 @@ import streamlit as st
 class Streamlit:
   def initiate_session_state(self, key, value):
     if key not in st.session_state:
-      st.session_state[key] = value  
+      st.session_state[key] = value
+    else: 
+      st.session_state[key] = value
